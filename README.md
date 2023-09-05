969ac191f59401a5beb9924a163ee13bc88af9c6
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2023-09-05 05:44:15 +0500

    add csv conversion capability

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## Run These Commands First:
+
 > python -m venv .venv
 
 > .\\.venv\Scripts\activate
 
 frozen dependencies from requirements, OR:
+
 > python -m pip install --upgrade --no-cache-dir -U pip --prefer-binary
 
 > python -m pip install --no-cache-dir -U PyPDF2 --prefer-binary
@@ -11,19 +13,21 @@ frozen dependencies from requirements, OR:
 > python -m pip install --no-cache-dir -U pandas --prefer-binary
 
 ## Project Structure:
+
 - .venv
-    -> create virtual environment with [python -m venv .venv] to keep packages local
+  -> create virtual environment with [python -m venv .venv] to keep packages local
 - JSON-output
-    -> guid-ed json output files will be here, can be more organized in further updates
+  -> guid-ed json output files will be here, can be more organized in further updates
 - PDF-input
-    -> place pdfs here, any file with *.pdf in this folder will be processed, stick to ASCII naming
+  -> place pdfs here, any file with \*.pdf in this folder will be processed, stick to ASCII naming
 - src
 - .gitignore
 - .python-version
-    -> use pyenv to trigger local version of python, HIGHLY RECOMMEND
+  -> use pyenv to trigger local version of python, HIGHLY RECOMMEND
 - README.md
 
 ## Description:
+
 Put all PDF files in PDF-input folder
 content inside JSON-output will be deleted,
 and all pdf files will be converted to json and output there.
@@ -32,11 +36,13 @@ ForNow, every JSON will have a guid,
 as file type specific naming is out of scope and rapid dev focus on business logic.
 
 ## Ignored Objects:
+
 - contents of PDF and JSONs, the folders are comitted but not the content
 - .venv scripts are comitted as well
 - pip package downloads inside script are ignored
 
 ## Notes:
+
 TODO: TRIGGERS EXCEPTION IF SCHEMA KEY DATA NOT AVAILABLE
 MAIN IDEA: divide into sections as dictionaries and iterate through data as lists
 processing of actual col data should be done in different program, this is only for reading raw data.
--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@
 # !python3 -m pip install --no-cache-dir -U PyPDF2 --prefer-binary
 # !python3 -m pip install --no-cache-dir -U pandas --prefer-binary
 
-# for non Jupyter environment, just remove the "!" from the above commands
-# if on unix/mac, replace python3 with python (depends, try python first, if not working, try python3)
-
 # READ README.md
 
 import os
@@ -18,7 +15,10 @@ import json
 import uuid
 
 input_dir = os.path.abspath(os.path.join("PDF-input"))
-output_dir = os.path.abspath(os.path.join("JSON-output"))
+output_dir_json = os.path.abspath(os.path.join("JSON-output"))
+output_dir_csv = os.path.abspath(os.path.join("CSV-output"))
+
+eod = "8th Floor, City Square, Chaandhanee Magu"
 
 form_info = {
     "Collateralization Returns": [ # strips "Employer Info" and flattens section
@@ -48,7 +48,26 @@ status_remarks = { # assumed, only "By" & "Date" in each section
         "Submitted"
     ]}
 
-form_schema = [form_info, collateralization, status_remarks]
+json_schema = [form_info, collateralization, status_remarks]
+
+# inner_item 1 is start delimiter, inner_item 2 is end delimiter,
+# if inner_item 2 is empty, will use next inner_item starting delimited as ending delimiter,
+# if last item's inner_item 2 is empty, end delimiter for that item will be EOD
+csv_schema  = [ 
+    ["Reference Number", ""],
+    ["Submitted By", ""],
+    ["Employer No", ""],
+    ["Employer Name", "Collateralization & Return Info"],
+    ["ID Card No", ""],
+    ["Full Name", ""],
+    ["Ref No", ""],
+    ["Collateralized Amount", ""],
+    ["Principle Amount", ""],
+    ["Balance Amount", ""],
+    ["Interest/Income Amount", ""],
+    ["Return from Principle Amount", ""],
+    ["Date of Return", "Status Remarks"]
+]
 
 class FormItemNotFoundExeception(Exception):
     def __init__(self, message="Form Item Not Found"):
@@ -65,6 +84,11 @@ def input_files() -> Generator[str, None, None]:
         if file.endswith(".pdf"):
             yield file
 
+# convert to extension method on typed str type
+def printing_message(file_name: str):
+    print(f"""
+  📚 -> CONVERTING: {file_name}""")
+
 def get_pdf_content(file_path: str) -> str:
     # print(file_path)
     pdf_content: str = "" # CONTENT OF PDF
@@ -78,8 +102,6 @@ def get_pdf_content(file_path: str) -> str:
                 pdf_content += page_content
     except Exception as err:
         print(f"Error: {err}")
-    with open('READ_DATA_TESTING.txt', 'w') as file:
-        file.write(pdf_content.replace("Employer Info", "", 1))
     return pdf_content.replace("Employer Info", "", 1)
 
 def section_seeker (schema_reference, section_data):
@@ -117,12 +139,12 @@ def convert_json(file_path: str):
     pdf_content_length = len(pdf_content)
     json_output = {}
     csp = 0 # cursor start position of pdf content
-    for section_index, form_section in enumerate(form_schema):
+    for section_index, form_section in enumerate(json_schema):
         key: str = re.sub(r'[{}":]', '', next(iter(form_section)))
         section = {key: []}
         section_start = re.search(f"{form_section[key][0]}", pdf_content).start() # TODO: switch to end + 1 and handle in case_1
         if (section_index != 2):
-            section_end   = re.search(f"{next(iter(form_schema[section_index + 1]))}", pdf_content).start() - 1
+            section_end   = re.search(f"{next(iter(json_schema[section_index + 1]))}", pdf_content).start() - 1
         else:
             section_end   = re.search("8th Floor, City Square, Chaandhanee Magu", pdf_content).start() - 1
         switch_case = {
@@ -134,16 +156,51 @@ def convert_json(file_path: str):
         json_output[key] = value
     return json_output
 
+# TODO: append to list asyncrhonously and write from list to disk in batch
+def convert_csv(file_path: str):
+    pdf_content = get_pdf_content(file_path)
+    csv_output = {}
+    for index, item in enumerate(csv_schema):
+        try:
+            delim_start = (re.search(item[0], pdf_content).end() + 1)
+            if (delim_start):
+                if (index != len(csv_schema) -1): # TODO: LOGIC HACK! CAREFUL  -- REFACTOR REFACTOR TODO: REFACTOR
+                    if (item[1] == ""):
+                        delim_end = re.search(csv_schema[index + 1][0], pdf_content).start() - 1
+                    else:
+                        # delim_end = match.start() - 1 if match else None
+                        delim_end = re.search(item[1], pdf_content).start() - 1
+                else:
+                    if (item[1] != ""):
+                        delim_end = delim_end = re.search(item[1], pdf_content).start() - 1
+                    else:
+                        delim_end = re.search(eod, pdf_content).start() - 1
+            else:
+                # TODO: schange terminatino scope to file only
+                raise FormItemNotFoundExeception(f"item: DELIM_START: {item}: NOT FOUND - Program Terminated") # exception overflow
+        except:
+            # TODO: add delim info and item and form info to err msg
+            raise FormItemNotFoundExeception(f"item: DELIM_END: {item}: NOT FOUND - Program Terminated") # fallover exception
+        csv_output[item[0]] = pdf_content[delim_start:delim_end].strip() # unhashable if key is list, make sure not indexable aka 1 value.
+    return csv_output
+
 # MAIN / TODO: Make Async
 output_type = input("Output Type: 1 (JSON) / 2 (CSV): ")
+
 if re.match(r'^[1|J|j]$', output_type, re.IGNORECASE):
     for file in input_files():
-        print(f"""
-            [ Initiated Conversion of {file} ]""")
+        printing_message(file)
         out = convert_json((os.path.join(input_dir, file)))
-        with open(os.path.join(output_dir, str(uuid.uuid4())+".json"), "w") as json_file:
+        with open(os.path.join(output_dir_json, str(uuid.uuid4())+".json"), "w") as json_file:
             json.dump(out, json_file)
+# TODO: create lock object on main list and generate add convert to pd DF then batch conver to csv using pd
 elif re.match(r'^[2|C|c]$', output_type, re.IGNORECASE):
-    raise NotImplementedError("CSV Conversion not fully implemented yet")
+    out = []
+    for file in input_files():
+        printing_message(file)
+        out.append(convert_csv((os.path.join(input_dir, file))))
+    df = pd.DataFrame(out)
+    with open(os.path.join(output_dir_csv, str(uuid.uuid4())+".csv"), "w") as csv_file:
+        df.to_csv(csv_file, index=False)
 else:
     raise InvalidInputError("Wrong input, Restart Program and Follow Instructions.")
